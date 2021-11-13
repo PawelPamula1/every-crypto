5bde80b24d81683155d26ec3b367d20eb7484de0
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-13 21:54:45 +0100

    hiding api key

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': '7d9fcdd503msh7ba679ffb6bc1a4p1006dbjsnbfe118d3cc2d',
+  'x-rapidapi-key': process.env.REACT_APP_API_KEY,
 };
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const cryptoNewsHeaders = {
   'x-bingapis-sdk': 'true',
   'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-  'x-rapidapi-key': '7d9fcdd503msh7ba679ffb6bc1a4p1006dbjsnbfe118d3cc2d',
+  'x-rapidapi-key': process.env.REACT_APP_NEWS_API_KEY,
 };
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
