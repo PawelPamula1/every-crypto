f13090d6b3dc73cf317fa18cb081a9dc9b3ee9eb
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-14 00:00:27 +0100

    everycrypto

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
             level={5}
             style={{ color: 'white', textAlign: 'center' }}
           >
-            Cryptoverse <br />
+            EveryCrypto <br />
             All rights reserved
           </Typography.Title>
           <Space>
